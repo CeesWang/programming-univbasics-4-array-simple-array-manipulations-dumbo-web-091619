0470a3ca94d468d0c0ee5c13e527a154520a72f6
Chen Wang <wangcees@gmail.com>
2019-08-14 18:26:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,21 @@ def shift_with_args(array)
 end
 
 def using_insert(array, element)
-  array.insert(element)
+  array.insert(element,4)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
 end
